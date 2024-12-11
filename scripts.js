7c6dd10e1d00ebf5cd5ce3cdac2db739cf6864a4
IncreibleJulk <nicoperez15@gmail.com>
2024-12-11 17:07:49 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,10 @@
+// script.js
+document.addEventListener('DOMContentLoaded', () => {
+    const buttons = document.querySelectorAll('.card button');
 
+    buttons.forEach(button => {
+        button.addEventListener('click', () => {
+            alert(`Has hecho clic en: ${button.parentElement.querySelector('h2').innerText}`);
+        });
+    });
+});
