dfb80d804abf673a7eca588301a1aa7f6be08a8d
increiblejulk <nicoperez15@gmail.com>
2025-05-20 18:58:54 -0300

    test 1

--- scripts.js
+++ scripts.js
@@ -1,10 +1,1 @@
-// script.js
-document.addEventListener('DOMContentLoaded', () => {
-    const buttons = document.querySelectorAll('.card button');
-
-    buttons.forEach(button => {
-        button.addEventListener('click', () => {
-            alert(`Has hecho clic en: ${button.parentElement.querySelector('h2').innerText}`);
-        });
-    });
-});
+console.log('start reactify web 1');
